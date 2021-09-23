eb1aeecd1fb3401b7efb8fa936c6c9373ddc135c
525061@chrome.escg.ac.uk <525061@chrome.escg.ac.uk>
2021-09-23 15:31:33 +0100

    no message

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace abc
             float dec = 6.25f;
             string word = "Malcolm Rubin";
             // use list instead of array
-            // int[] array1 = new int[5];
-            // int[] array2 = { 1, 2, 3, 4, 5 };
+            /* int[] array1 = new int[5];
+            int[] array2 = { 1, 2, 3, 4, 5 }; */
             Console.WriteLine("Hello World! My name is " + word + " and I am " + number + " years old and am " + dec + " feet tall.");
         }
     }

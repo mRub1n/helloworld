cbbeb815d6bbe0da2a2a53e025a2a5094ae92554
525061@chrome.escg.ac.uk <525061@chrome.escg.ac.uk>
2021-09-23 14:48:47 +0100

    experimenting with int, float, string, array and list in visual studios

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace abc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 17;
+            float dec = 6.25f;
+            string word = "Malcolm Rubin";
+            // use list instead of array
+            // int[] array1 = new int[5];
+            // int[] array2 = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Hello World! My name is " + word + " and I am " + number + " years old and am " + dec + " feet tall.");
         }
     }
 }
